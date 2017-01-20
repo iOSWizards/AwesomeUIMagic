5760eba8f3e9b763678ff93760e46ee4097095f7
Evandro Harrison Hoffmann <ehh85@yahoo.com.br>
2017-01-20 11:16:48 +0800

    Initial commit

---
+++ Example/AwesomeUIMagic/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AwesomeUIMagic
+//
+//  Created by Evandro Harrison Hoffmann on 01/20/2017.
+//  Copyright (c) 2017 Evandro Harrison Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
